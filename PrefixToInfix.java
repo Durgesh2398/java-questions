c9220975bc564087b563c7c8636043cbe6f81d19
Durgesh2398 <2308durgesh@gmail.com>
2019-12-13 23:02:31 +0530

    prefix to infix

---
+++ PrefixToInfix.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+public class PrefixToInfix {
+    public String convert(String expression){
+        Stack<String> stack = new Stack<>();
+        for (int i = expression.length()-1; i >=0 ; i--) {
+            char c = expression.charAt(i);
+            if(isOperator(c)){
+                String s1 = stack.pop();
+                String s2 = stack.pop();
+                String temp = "("+s1+c+s2+")";
+                stack.push(temp);
+            }else{
+                stack.push(c+"");
+            }
+        }
+        String result=stack.pop();
+        return result;
+    }
+    boolean isOperator(char x) {
+        switch (x) {
+            case '+':
+            case '-':
+            case '/':
+            case '*':
+                return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        PrefixToInfix obj = new PrefixToInfix();
+        String exp = "*-A/BC-/AKL";
+        System.out.println("Prefix Expression: " + exp);
+        System.out.println("Infix Expression: " + obj.convert(exp));
+    }
+}
